b6a643c0b6536ef6e58e5be15007e76862648db7
Thomas-Saleh <s4006031@student.rmit.edu.au>
2024-05-25 23:34:15 +1000

    test commit

--- s4006031-s3928350-a2/React/src/pages/VegetableTips.js
+++ s4006031-s3928350-a2/React/src/pages/VegetableTips.js
@@ -11,13 +11,7 @@ function VegetableTips() {
       <div className="tips-overlay w-1/3 text-black">
         <h2 className="text-2xl font-semibold mb-4">Here are some Tips and Tricks for Growing Small Vegetables in Your Backyard:</h2>
         <ul className="list-disc pl-6">
-          <li>Select an appropriate spot for your vegetable garden that receives sufficient sunlight.</li>
-          <li>If you have limited space, think about using raised beds or containers.</li>
-          <li>Choose your vegetables wisely. Some love direct sunlight whilst others prefer shade.</li>
-          <li>Ensure you are regularly watering and fertilizing to promote healthy growth of your plants.</li>
-          <li>Make sure to not drown your veggie plants, it shouldn't be too soggy.</li>
-          <li>Keep up with hygiene to prevent pests and diseases from ruining your vegetables.</li>
-          <li>Harvest your vegetables at the right time, not too early not too late encourage ongoing production.</li>
+          
         </ul>
       </div>
     </div>
